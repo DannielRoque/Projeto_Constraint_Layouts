04367a46807205515d9d9b957a66187a81c3d237
Daniel <daniel.roque2@fatec.sp.gov.br>
2019-10-18 11:59:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/danielviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,14 @@
+package com.example.danielviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.danielviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
