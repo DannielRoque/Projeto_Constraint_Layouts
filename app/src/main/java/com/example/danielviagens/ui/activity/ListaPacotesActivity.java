616b55ab7fd93c7b1d97e5953a92c17db8262d81
Daniel <daniel.roque2@fatec.sp.gov.br>
2019-10-28 11:25:41 -0300

    Conforme nova necessidade a apmliação do projeto adicionado resumo do pacote

--- app/src/main/java/com/example/danielviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/example/danielviagens/ui/activity/ListaPacotesActivity.java
@@ -1,5 +1,6 @@
 package com.example.danielviagens.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -20,6 +21,9 @@ public class ListaPacotesActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
 
         configuraLista();
+
+        Intent intent = new Intent(this, ResumoPacoteActivity.class);
+        startActivity(intent);
     }
 
     private void configuraLista() {
---
+++ app/src/main/java/com/example/danielviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,67 @@
+package com.example.danielviagens.ui.activity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.danielviagens.R;
+import com.example.danielviagens.model.Pacote;
+import com.example.danielviagens.util.DataUtil;
+import com.example.danielviagens.util.DiasUtil;
+import com.example.danielviagens.util.ImagensUtil;
+import com.example.danielviagens.util.MoedasUtil;
+
+import java.math.BigDecimal;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo do Pacote";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraDias(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(dataFormatadaDaViagem);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String formataParaBrasileiro = MoedasUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(formataParaBrasileiro);
+    }
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.diasEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ImagensUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_pacote_cidade);
+        local.setText(pacote.getLocal());
+    }
+}
