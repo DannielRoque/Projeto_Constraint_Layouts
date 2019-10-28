9d6ffa892e56261836c810ae45fbf23f3094d115
Daniel <daniel.roque2@fatec.sp.gov.br>
2019-10-28 12:06:39 -0300

    iniciando o formulario de pagamento

---
+++ app/src/main/java/com/example/danielviagens/ui/activity/PagamentoActivity.java
@@ -0,0 +1,16 @@
+package com.example.danielviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.danielviagens.R;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
